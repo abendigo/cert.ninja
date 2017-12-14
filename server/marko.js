9ff941d2dfa1450c34ed3a85c0e41f5e35c3315f
Doug Hoyte <doug@hcsw.org>
2017-12-13 21:22:24 -0500

    clean-up

--- server/marko.js
+++
@@ -1,9 +0,0 @@
-const validators = require('./validators.js');
-
-validators.fetchWellKnownFile('cert.ninja', (err, res) => {
-  console.log('0', err, res)
-});
-
-validators.validateDomain('cert.ninja', 'Wazzzup People!\n', (err, res) => {
-  console.log('1', err, res)
-})
